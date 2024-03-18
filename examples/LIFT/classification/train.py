d636a7c12fa6f78b1d9ef0264844b80e6b0acfc1
dajiaohuang <mikewushuwen@outlook.com>
2024-03-18 22:07:20 +0800

    lift

--- examples/LIFT/classification/train.py
+++ examples/LIFT/classification/train.py
@@ -83,7 +83,7 @@ gpt = GPTJ.LoRaQGPTJ(adapter=True, device=device)
 train_configs={'learning_rate': 1e-5, 'batch_size': 1, 'epochs':1,  'weight_decay': 0.01, 'warmup_steps': 6}
 gpt.finetune('data/train.json', 'data/val.json', train_configs, saving_checkpoint=False)
 pred= query(gpt, test_prompts,bs=8)
-write_jsonl(pred,'pred.json')
+write_jsonl('\n'.join(pred),'pred.json')
 y_pred = pd.DataFrame([x.split('"')[-1].split("|") for x in pred])
 
 # print(y_pred)
