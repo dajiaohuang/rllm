d7c9c9423761778b99411b2f52e3502ce5157e40
dajiaohuang <mikewushuwen@outlook.com>
2024-03-20 16:50:17 +0800

    clf not finetune

--- examples/LIFT/classification/train.py
+++ examples/LIFT/classification/train.py
@@ -85,7 +85,7 @@ print(y_test)
 
 gpt = GPTJ.LoRaQGPTJ(adapter=True, device=device)
 train_configs={'learning_rate': 1e-5, 'batch_size': 1, 'epochs':1,  'weight_decay': 0.01, 'warmup_steps': 6}
-gpt.finetune('data/train.json', 'data/val.json', train_configs, saving_checkpoint=False)
+# gpt.finetune('data/train.json', 'data/val.json', train_configs, saving_checkpoint=False)
 pred = query(gpt, test_prompts,bs=8)
 # write_jsonl('\n'.join(pred),'pred.json')
 print(pred)
