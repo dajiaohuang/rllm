9c9ce8086fd5669a22b92848c77ffd4625295bf3
dajiaohuang <mikewushuwen@outlook.com>
2024-03-21 20:01:25 +0800

    reg

--- examples/LIFT/regression/train.py
+++ examples/LIFT/regression/train.py
@@ -27,8 +27,7 @@ def df2prompts(df:pd.DataFrame, init = '',end = '',prompts_each_user = 1, n_give
         selected_groups = [grouped.get_group(user[0]) for user in selected_users]
         # print(selected_groups)
     else:
-        selected_users = grouped['UserID'].unique().sample(n=grouped.size(), replace=False)
-        selected_groups = [grouped.get_group(user[0]) for user in selected_users]
+        selected_groups = [group for _, group in grouped]
         
     jsonl = []
     for group in selected_groups:
